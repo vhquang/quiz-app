27fde85e5549e5dd33d6a6f21f840591230142ef
Quang Vu <vhquang@gmail.com>
2017-07-29 22:18:24 -0700

    implement validating input json file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import './index.css';
 
 // registerServiceWorker();
 
-// todo write validator
 let list = [
   {
     "question": "question 1",
@@ -36,6 +35,44 @@ let list = [
     "explanation": "explain 2"
   },
 ];
+
+const dataSchema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "description": "A list of questions for quiz",
+  "type": "array",
+  "minItems": 1,
+  "items": {
+    "$ref": "#/definitions/questionObject"
+  },
+  "uniqueItems": true,
+  "definitions": {
+    "questionObject": {
+      "type": "object",
+      "properties": {
+        "question": {
+          "type": "string"
+        },
+        "explanation": {
+          "type": "string"
+        },
+        "correct": {
+          "type": "array",
+          "items": { "type": "string" },
+          "minItems": 1,
+          "uniqueItems": true
+        },
+        "wrong": {
+          "type": "array",
+          "items": { "type": "string" },
+          "minItems": 1,
+          "uniqueItems": true
+        }
+      },
+      "required": ["correct", "wrong"]
+    }
+  }
+};
+
 
 function shuffle(a) {
   for (let i = a.length; i; i--) {
@@ -324,17 +361,32 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      dataErrorMsg: null
     };
+    const Ajv = require('ajv');
+    const ajv = new Ajv({allErrors: true});
+    ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
+    this.ajv = ajv;
     this.handleFileSelect = this.handleFileSelect.bind(this);
   }
 
-  displayData(content) {
+  validateData(content) {
+    // todo try/catch
     const json = JSON.parse(content);
-    this.setState({data: json});
+    if (this.ajv.validate(dataSchema, json)) {
+      this.setState( {data: json} );
+    } else {
+      console.log(this.ajv.errorsText());
+      this.setState( {dataErrorMsg: this.ajv.errorsText()} );
+    }
   }
 
   handleFileSelect(evt) {
+    this.setState({
+      data: null,
+      dataErrorMsg: null
+    });
     let files = evt.target.files;
     if (!files.length) {
       alert('No file select');
@@ -344,8 +396,7 @@ class App extends React.Component {
     let that = this;
     let reader = new FileReader();
     reader.onload = function(e) {
-      console.log(e.target.result);
-      that.displayData(e.target.result);
+      that.validateData(e.target.result);
     };
     reader.readAsText(file);
   }
@@ -364,9 +415,17 @@ class App extends React.Component {
         {uploadFile}
       </div>
     );
+    const dataErrorMsg = this.state.dataErrorMsg;
+    const errorPanel = (
+      <div className="container well panel" style={{"textAlign": "center"}}>
+        <p>{dataErrorMsg}</p>
+      </div>
+    );
+
     return (
       <div>
       { !data && uploadPanel }
+      { dataErrorMsg && errorPanel }
       { data && <Quiz questions={data} /> }
       </div>
     );
